264cb23a46925334a2e2097c9a4900ca3abd5ffe
ajnikgunu <57931279+ajnikgunu@users.noreply.github.com>
2019-12-11 16:28:31 +0545

    My Input Change

--- src/inputField.jsx
+++ src/inputField.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
-const InputField = props => {
-  return <div>This is test</div>;
+const InputField = ({ name, label, register, errors }) => {
+  return (
+    <>
+      <label htmlFor={name}>{label}</label>
+      <input
+        name={name}
+        placeholder="..."
+        ref={register({ required: true })}
+        className={cn("form-input", {
+          "is-error": errors[name]
+        })}
+      />
+      {errors[name] &&
+        errors[name].type === "required" &&
+        "This field is required"}
+    </>
+  );
 };
 
 export default InputField;
